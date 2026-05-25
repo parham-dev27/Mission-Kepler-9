6c28ac444314fc8fb164d5b66340e825a11b11f8
parham <navabiparham@gmail.com>
2026-05-25 16:05:27 +0200

    Day 2 - Zusammenfassung fertig

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from random import randint
 NAME: str = "ARES-7"
 START_POS = {"x": 0, "y": 0}
 START_WINKEL: int = 0
-SCHWIERIGKEITSGRAD = 0
+SCHWIERIGKEITSGRAD: str = "mittel"
 
 
 def intro() -> None:
@@ -33,6 +33,7 @@ def parameter() -> None:
     """
         NAME
     """
+    global NAME
     print()
     centerText("[1/4] NAME DES ROVERS", "=")
     for i in range(1, 4):
@@ -55,6 +56,7 @@ def parameter() -> None:
     """
         START POSITION
     """
+    global START_POS
     print()
     centerText("[2/4] START POSITION", "=")
     print("Wo startet der Rover?")
@@ -62,7 +64,7 @@ def parameter() -> None:
         try:
             pos_x: int = int(input(
                 "Start X (Erlaubt: -100 bis 100): ").strip())
-            if pos_x and pos_x in range(-100, 101):
+            if str(pos_x) and pos_x in range(-100, 101):
                 break
             else:
                 raise ValueError
@@ -81,7 +83,7 @@ def parameter() -> None:
         try:
             pos_y: int = int(input(
                 "Start Y (Erlaubt: -100 bis 100): ").strip())
-            if pos_y and pos_y in range(-100, 101):
+            if str(pos_y) and pos_y in range(-100, 101):
                 break
             else:
                 raise ValueError
@@ -102,6 +104,7 @@ def parameter() -> None:
     """
         START WINKEL
     """
+    global START_WINKEL
     print()
     centerText("[3/4] START WINKEL", "=")
     print("In welche Richtung schaut der Rover beim START?")
@@ -109,7 +112,7 @@ def parameter() -> None:
     for i in range(1, 4):
         winkel = input(
             "Startwinkel (0–359) oder Richtung (N/S/O/W): ").strip().upper()
-        if winkel and winkel in RICHTUNGEN:
+        if str(winkel) and winkel in RICHTUNGEN:
             START_WINKEL = RICHTUNGEN[winkel]
             break
         try:
@@ -133,6 +136,7 @@ def parameter() -> None:
     """
         SCHWIERIGKEITSGRAD
     """
+    global SCHWIERIGKEITSGRAD
     print()
     centerText("[4/4] SCHWIERIGKEITSGRAD", "=")
     print("Wie schwer willst du es haben?")
@@ -156,8 +160,10 @@ def parameter() -> None:
 
     for i in range(1, 4):
         try:
-            SCHWIERIGKEITSGRAD = int(input("Schwierigkeit (1/2/3): ").strip())
-            if SCHWIERIGKEITSGRAD and SCHWIERIGKEITSGRAD in range(1, 4):
+            grad = int(input("Schwierigkeit (1/2/3): ").strip())
+            if str(grad) and grad in range(1, 4):
+                SCHWIERIGKEITSGRAD = list(SCHWIERIGKEITSGRAD_KRITERIEN.keys())[
+                    grad - 1]
                 break
             raise ValueError
         except ValueError:
@@ -166,7 +172,8 @@ def parameter() -> None:
                 print(
                     f"Bitte geben Sie einen gültigen Wert ein. (Noch {3 - i} Versuche)")
             else:
-                SCHWIERIGKEITSGRAD = 2
+                SCHWIERIGKEITSGRAD = list(
+                    SCHWIERIGKEITSGRAD_KRITERIEN.keys())[1]
                 print()
                 print("Üngültig")
     print()
@@ -177,15 +184,31 @@ def zusammenfassung():
     h_line()
     centerText("Zusammenfassung")
     h_line()
+    centerText(f"Rover-Name: {NAME.upper()}")
+    centerText(f"Startposition: ({START_POS['x']}|{START_POS['y']})")
+    centerText(f"Startwinkel: {START_WINKEL}°")
+    centerText(
+        f"Schwierigkeit: {SCHWIERIGKEITSGRAD.capitalize()}")
+    h_line()
+    print("Fortfahren (Ja/Nein)? ", end="")
+    if input("").lower() in JA:
+        return
+    else:
+        raise ConnectionRefusedError
 
 
 def main() -> None:
-    # clear()
-    # intro()
-    # clear()
-    # parameter()
     clear()
-    zusammenfassung()
+    intro()
+    while True:
+        try:
+            clear()
+            parameter()
+            clear()
+            zusammenfassung()
+            break
+        except ConnectionRefusedError:
+            continue
 
 
 if __name__ == "__main__":
@@ -197,4 +220,4 @@ if __name__ == "__main__":
         print("Abbruch...")
         exit()
     except Exception as E:
-        print(E)
+        raise Exception
